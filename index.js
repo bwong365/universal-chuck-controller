6cc889539a68c6c2455107bf2328b0d2a324a2ec
Ben Wong <bwong365@outlook.com>
2018-10-13 14:52:01 -0600

    triple the routes!

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const port = process.env.PORT || 3000;
 app.listen(port);
 
 app.route('/').get(index);
-//app.route('/chuck')
-
+app.route('/chuck').get((req, res) => getChuck(req, res, false));
+app.route('/chuck/:translate').get((req, res) => getChuck(req, res, true));
 
 function index(req, res) {
   res.send('Hi!');
+}
+
+function getChuck(req, res, translate) {
+  if (translate) console.log('translating');
+  console.log(req.query);
 }
