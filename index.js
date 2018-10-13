1c9c0bf7624a76dbba3410a1680095ffc03255c5
Ben Wong <bwong365@outlook.com>
2018-10-13 15:27:02 -0600

    gets Chuck lvl 1

--- index.js
+++ index.js
@@ -4,17 +4,38 @@ const axios = require('axios');
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.route('/').get(index);
 app.route('/chuck').get((req, res) => getChuck(req, res, false));
 app.route('/chuck/:translate').get((req, res) => getChuck(req, res, true));
 
+// learning is fun!
+app.get('/test', (req, res) => {
+  res.send(req.query);
+}).get('/test/:text', (req, res) => {
+  res.send(req.params);
+});
+
+
 function index(req, res) {
   res.send('Hi!');
 }
 
+// Route both requests through here
 function getChuck(req, res, translate) {
-  if (translate) console.log('translating');
-  console.log(req.query);
+  console.log(translate ? 'translate hit!' : 'hit!')
+  const chuckAPI = 'https://api.chucknorris.io/jokes/random';
+  const langAPI = 'https://api.funtranslations.com/translate';
+  const lang = req.params;
+  const text = req.query.text;
+
+  axios.get(chuckAPI)
+    .then(fact => {
+      res.send(fact.data.value);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(300).send(err);
+    });
 }
